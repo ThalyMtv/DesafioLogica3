66dee3aef833b4054cfc7951d5983eb7f3964d4d
ThalyMtv <thalitymtv@gmail.com>
2024-10-27 15:14:38 -0300

    Desafio escrevendo as classes de um jogo

---
+++ index.js
@@ -0,0 +1,31 @@
+class heroi{
+    constructor(nome,idade,tipo){
+        this.nome = nome
+        this.idade = idade
+        this.tipo = tipo
+    }
+
+    atacar(){
+        let ataque
+        switch(this.tipo){
+            case "mago":
+                ataque = "magia"
+            break
+            case "guerreiro":
+                ataque = "espada"
+            break
+            case "monge":
+                ataque = "artes marciais"
+            break
+            case "ninja":
+                ataque = "shuriken"
+            break
+        }
+        console.log(`- O ${this.tipo} atacou usando ${ataque}`)
+    }
+}
+
+
+let nomeHeroi = new heroi("Link", 18, "ninja")
+
+nomeHeroi.atacar()
